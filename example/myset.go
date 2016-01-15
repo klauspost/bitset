ffb3e3e4e74734c4992d52b1a6f96cc89b722b43
klauspost <klauspost@gmail.com>
2016-01-15 15:51:14 +0100

    Update receiver name.

--- example/myset.go
+++ example/myset.go
@@ -15,70 +15,70 @@ var _ = MySet(MyType(0))
 
 type MySet int
 
-// NewMySet will create a new hash set with the hash types supplied
+// NewMySet will create a new MyType set with the MyType types supplied
 func NewMySet(t ...MyType) MySet {
-	h := MySet(0)
-	return h.Set(t...)
+	x := MySet(0)
+	return x.Set(t...)
 }
 
-// Set one or more hash types to the set.
-// Returns the modified hash set.
-func (h *MySet) Set(t ...MyType) MySet {
+// Set one or more MyType types to the set.
+// Returns the modified MyType set.
+func (x *MySet) Set(t ...MyType) MySet {
 	for _, v := range t {
-		*h |= MySet(v)
+		*x |= MySet(v)
 	}
-	return *h
+	return *x
 }
 
 // Clear one or more types from the set.
-// Returns the modified hash set.
-func (h *MySet) Clear(t ...MyType) MySet {
+// Returns the modified MyType set.
+func (x *MySet) Clear(t ...MyType) MySet {
 	for _, v := range t {
-		*h &= ^MySet(v)
+		*x &= ^MySet(v)
 	}
-	return *h
+	return *x
 }
 
 // Toggle one or more types from the set.
-// Returns the modified hash set.
-func (h *MySet) Toggle(t ...MyType) MySet {
+// Returns the modified MyType set.
+func (x *MySet) Toggle(t ...MyType) MySet {
 	for _, v := range t {
-		*h ^= MySet(v)
+		*x ^= MySet(v)
 	}
-	return *h
+	return *x
 }
 
 // Contains returns true if the
-func (h MySet) Contains(t MyType) bool {
-	return int(h)&int(t) != 0
+func (x MySet) Contains(t MyType) bool {
+	return int(x)&int(t) != 0
 }
 
 // Overlap returns the overlapping set bits
-func (h MySet) Overlap(t MySet) MySet {
-	return MySet(int(h) & int(t))
+func (x MySet) Overlap(t MySet) MySet {
+	return MySet(int(x) & int(t))
 }
 
 // Combine returns the combined bit sets
-func (h MySet) Combine(t MySet) MySet {
-	return MySet(int(h) | int(t))
+func (x MySet) Combine(t MySet) MySet {
+	return MySet(int(x) | int(t))
 }
 
 // Same returns the bit values that are the same
-func (h MySet) Same(t MySet) MySet {
-	return MySet(int(h) ^ int(t))
+func (x MySet) Same(t MySet) MySet {
+	return MySet(int(x) ^ int(t))
 }
 
 // SubsetOf will return true if all types of h
 // is present in the set c
-func (h MySet) SubsetOf(c MySet) bool {
-	return int(h)|int(c) == int(c)
+func (x MySet) SubsetOf(y MySet) bool {
+	return int(x)|int(y) == int(y)
 }
 
-// GetOne will return a hash type.
+// GetOne will return a MyType type.
 // Currently the first is returned, but it could be
 // improved to return the strongest.
-func (h MySet) GetOne() MyType {
-	v := int(h)
+func (x MySet) GetOne() MyType {
+	v := int(x)
 	i := uint(0)
 	for v != 0 {
 		if v&1 != 0 {
@@ -90,9 +90,9 @@ func (h MySet) GetOne() MyType {
 	return MyType(0)
 }
 
-// Array returns an array of all hash types in the set
-func (h MySet) Array() (ht []MyType) {
-	v := uint64(h)
+// Array returns an array of all MyType types in the set
+func (x MySet) Array() (ht []MyType) {
+	v := uint64(x)
 	i := uint(0)
 	for v != 0 {
 		if v&1 != 0 {
@@ -104,24 +104,24 @@ func (h MySet) Array() (ht []MyType) {
 	return ht
 }
 
-// Count returns the number of hash types in the set
-func (h MySet) Count() int {
-	if int(h) == 0 {
+// Count returns the number of MyType types in the set
+func (x MySet) Count() int {
+	if int(x) == 0 {
 		return 0
 	}
 	// credit: https://code.google.com/u/arnehormann/
-	x := uint64(h)
-	x -= (x >> 1) & 0x5555555555555555
-	x = (x>>2)&0x3333333333333333 + x&0x3333333333333333
-	x += x >> 4
-	x &= 0x0f0f0f0f0f0f0f0f
-	x *= 0x0101010101010101
-	return int(x >> 56)
+	y := uint64(x)
+	y -= (y >> 1) & 0x5555555555555555
+	y = (y>>2)&0x3333333333333333 + y&0x3333333333333333
+	y += y >> 4
+	y &= 0x0f0f0f0f0f0f0f0f
+	y *= 0x0101010101010101
+	return int(y >> 56)
 }
 
-// String returns a string representation of the hash set.
-func (h MySet) String() string {
-	a := h.Array()
+// String returns a string representation of the MyType set.
+func (x MySet) String() string {
+	a := x.Array()
 	var r []string
 	for _, v := range a {
 		s := fmt.Sprintf("%v", v)
